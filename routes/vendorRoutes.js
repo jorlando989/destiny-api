68e3d38e99d13342699c044e7253ab5a7a2aa62d
jorlando989 <jorlando989@gmail.com>
2022-08-12 15:54:02 -0400

    comment

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -4,7 +4,7 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => { 
     app.get("/api/vendors", requireLogin, async (req, res) => {
-        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        // const currentUser = JSON.parse(localStorage.getItem('currentUser'));
         // const query = `https://www.bungie.net/Platform/Destiny2/2/Profile/${currentUser.membership_id}/Character/${characterID}/Vendors/?components=400,402`;
         
         // const {vendorGroups, vendors} = await fetch(query);
