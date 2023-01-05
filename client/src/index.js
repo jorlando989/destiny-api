65ab83a35a0b828843c11324b880eb12c8a46b63
jorlando989 <jorlando989@gmail.com>
2023-01-05 12:35:56 -0500

    refactor to configure store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reduxThunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, {}, composeEnhancers( 
-  applyMiddleware(reduxThunk)
-));
+
+const store = configureStore({
+    reducer: reducers, 
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxThunk), 
+    devTools: process.env.NODE_ENV !== 'production'
+  });
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
