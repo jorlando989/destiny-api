091b830a53b4abc3a0b94092771ccc7c645faae7
jorlando989 <jorlando989@gmail.com>
2023-01-10 14:14:31 -0500

    move prod setup code

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ checkForNewManifestVersion().then(() => {
   require('./routes/vendorRoutes')(app);
   require('./routes/activityRoutes')(app);
   require('./routes/recordsRoutes')(app);
+
+  if (process.env.NODE_ENV === 'production') {
+    //ensure Express serves up production assets
+    app.use(express.static('client/build'));
+  
+    //Express will serve index.html file if route not recognized, for react-router to handle
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT);
 });
 
 //check manifest version every 5 minutes
 var manifestInterval = setInterval(() => checkForNewManifestVersion(), 60*5000);
-
-if (process.env.NODE_ENV === 'production') {
-  //ensure Express serves up production assets
-  app.use(express.static('client/build'));
-
-  //Express will serve index.html file if route not recognized, for react-router to handle
-  const path = require('path');
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
